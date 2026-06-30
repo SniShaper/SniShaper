19f333263fb4d192c986354c76e1b47359352eae
dongle <youmao2023@outlook.com>
2026-06-30 21:11:20 +0800

    图标: heroicons替换iconify, 本地瞬时加载, 换风格

--- frontend/src/lib/icons.tsx
+++ frontend/src/lib/icons.tsx
@@ -1,83 +1,110 @@
-import { Icon as IconifyIcon } from '@iconify/react';
-import React from 'react';
+import React from 'react'
 
-function L(name: string) {
-  return React.forwardRef<SVGSVGElement, any>(({ size, ...props }, ref) => (
-    <IconifyIcon ref={ref} icon={`lucide:${name}`} width={size || 24} {...props} />
-  ));
+type H = React.ComponentType<React.SVGProps<SVGSVGElement>>
+
+function h(Icon: H) {
+  return React.forwardRef<SVGSVGElement, any>(({ size, className, ...props }, ref) => (
+    <Icon ref={ref} width={size || 24} height={size || 24} className={className} {...props} />
+  ))
 }
 
-function LN(name: string) {
-  return L(name);
-}
+import {
+  ArrowDownIcon, ArrowDownRightIcon, ArrowRightIcon, ArrowUpIcon, ArrowUpRightIcon,
+  ArrowPathIcon, ArrowPathRoundedSquareIcon, ArrowTopRightOnSquareIcon,
+  ArrowDownTrayIcon,
+  Bars3Icon, BellAlertIcon, BoltIcon,
+  CheckCircleIcon, ChevronDownIcon, ChevronUpIcon,
+  ClockIcon, CloudIcon, CodeBracketIcon, ComputerDesktopIcon,
+  CogIcon, CpuChipIcon,
+  DocumentCheckIcon, DocumentTextIcon,
+  ExclamationCircleIcon,
+  FolderOpenIcon, FunnelIcon,
+  GlobeAltIcon,
+  HeartIcon,
+  InformationCircleIcon,
+  LanguageIcon, LifebuoyIcon, LinkIcon as HeroLinkIcon, LockClosedIcon,
+  MagnifyingGlassIcon, MapIcon, MegaphoneIcon, MinusIcon, MoonIcon,
+  PauseIcon, PencilIcon, PlayIcon, PlusIcon, PlusCircleIcon, PowerIcon,
+  RectangleGroupIcon,
+  ServerIcon, ShareIcon, ShieldCheckIcon, ShieldExclamationIcon,
+  SignalIcon, SparklesIcon, Squares2X2Icon, SunIcon,
+  TrashIcon,
+  UserGroupIcon,
+  WifiIcon,
+  XCircleIcon, XMarkIcon,
+} from '@heroicons/react/24/outline'
 
-export const Activity = L('activity');
-export const AlertCircle = L('alert-circle');
-export const Anchor = L('anchor');
-export const Antenna = L('antenna');
-export const ArrowDown = L('arrow-down');
-export const ArrowDownRight = L('arrow-down-right');
-export const ArrowRight = L('arrow-right');
-export const ArrowUp = L('arrow-up');
-export const ArrowUpRight = L('arrow-up-right');
-export const BellRing = L('bell-ring');
-export const CheckCircle2 = L('check-circle-2');
-export const CheckSquare = L('check-square');
-export const ChevronDown = L('chevron-down');
-export const ChevronsUp = L('chevrons-up');
-export const ChevronUp = L('chevron-up');
-export const Cloud = L('cloud');
-export const CloudLightning = L('cloud-lightning');
-export const Code2 = L('code-2');
-export const Cpu = L('cpu');
-export const Download = L('download');
-export const Edit3 = L('edit-3');
-export const ExternalLink = L('external-link');
-export const FileText = L('file-text');
-export const Filter = L('filter');
-export const FolderOpen = L('folder-open');
-export const GitBranch = L('git-branch');
-export const Globe = L('globe');
-export const Heart = L('heart');
-export const History = L('history');
-export const Info = L('info');
-export const Languages = L('languages');
-export const LayoutDashboard = L('layout-dashboard');
-export const Link = L('link');
-export const LinkIcon = L('link');
-export const Loader2 = L('loader-2');
-export const Lock = L('lock');
-export const Map = L('map');
-export const Megaphone = L('megaphone');
-export const Menu = L('menu');
-export const Minus = L('minus');
-export const Monitor = L('monitor');
-export const Moon = L('moon');
-export const Network = L('network');
-export const Pause = L('pause');
-export const Play = L('play');
-export const Plus = L('plus');
-export const PlusCircle = L('plus-circle');
-export const Power = L('power');
-export const Radio = L('radio');
-export const RefreshCcw = L('refresh-ccw');
-export const RefreshCw = L('refresh-cw');
-export const Save = L('save');
-export const Search = L('search');
-export const Server = L('server');
-export const Settings = L('settings');
-export const Share2 = L('share-2');
-export const Shield = L('shield');
-export const ShieldAlert = L('shield-alert');
-export const ShieldCheck = L('shield-check');
-export const Sparkles = L('sparkles');
-export const Square = L('square');
-export const Sun = L('sun');
-export const Timer = L('timer');
-export const Trash2 = L('trash-2');
-export const Users = L('users');
-export const Wifi = L('wifi');
-export const Workflow = L('workflow');
-export const X = L('x');
-export const XCircle = L('x-circle');
-export const Zap = L('zap');
+import {
+  CheckBadgeIcon as CheckSquareSolid,
+} from '@heroicons/react/24/solid'
+
+export const Activity = h(RectangleGroupIcon)
+export const AlertCircle = h(ExclamationCircleIcon)
+export const Anchor = h(LifebuoyIcon)
+export const Antenna = h(SignalIcon)
+export const ArrowDown = h(ArrowDownIcon)
+export const ArrowDownRight = h(ArrowDownRightIcon)
+export const ArrowRight = h(ArrowRightIcon)
+export const ArrowUp = h(ArrowUpIcon)
+export const ArrowUpRight = h(ArrowUpRightIcon)
+export const BellRing = h(BellAlertIcon)
+export const CheckCircle2 = h(CheckCircleIcon)
+export const CheckSquare = h(CheckSquareSolid)
+export const ChevronDown = h(ChevronDownIcon)
+export const ChevronsUp = h(ChevronUpIcon)
+export const ChevronUp = h(ChevronUpIcon)
+export const Cloud = h(CloudIcon)
+export const CloudLightning = h(BoltIcon)
+export const Code2 = h(CodeBracketIcon)
+export const Cpu = h(CpuChipIcon)
+export const Download = h(ArrowDownTrayIcon)
+export const Edit3 = h(PencilIcon)
+export const ExternalLink = h(ArrowTopRightOnSquareIcon)
+export const FileText = h(DocumentTextIcon)
+export const Filter = h(FunnelIcon)
+export const FolderOpen = h(FolderOpenIcon)
+export const GitBranch = h(ArrowPathRoundedSquareIcon)
+export const Globe = h(GlobeAltIcon)
+export const Heart = h(HeartIcon)
+export const History = h(ClockIcon)
+export const Info = h(InformationCircleIcon)
+export const Languages = h(LanguageIcon)
+export const LayoutDashboard = h(RectangleGroupIcon)
+export const Link = h(HeroLinkIcon)
+export const LinkIcon = h(HeroLinkIcon)
+export const Loader2 = h(ArrowPathIcon)
+export const Lock = h(LockClosedIcon)
+export const Map = h(MapIcon)
+export const Megaphone = h(MegaphoneIcon)
+export const Menu = h(Bars3Icon)
+export const Minus = h(MinusIcon)
+export const Monitor = h(ComputerDesktopIcon)
+export const Moon = h(MoonIcon)
+export const Network = h(SignalIcon)
+export const Pause = h(PauseIcon)
+export const Play = h(PlayIcon)
+export const Plus = h(PlusIcon)
+export const PlusCircle = h(PlusCircleIcon)
+export const Power = h(PowerIcon)
+export const Radio = h(SignalIcon)
+export const RefreshCcw = h(ArrowPathRoundedSquareIcon)
+export const RefreshCw = h(ArrowPathIcon)
+export const Save = h(DocumentCheckIcon)
+export const Search = h(MagnifyingGlassIcon)
+export const Server = h(ServerIcon)
+export const Settings = h(CogIcon)
+export const Share2 = h(ShareIcon)
+export const Shield = h(ShieldCheckIcon)
+export const ShieldAlert = h(ShieldExclamationIcon)
+export const ShieldCheck = h(ShieldCheckIcon)
+export const Sparkles = h(SparklesIcon)
+export const Square = h(Squares2X2Icon)
+export const Sun = h(SunIcon)
+export const Timer = h(ClockIcon)
+export const Trash2 = h(TrashIcon)
+export const Users = h(UserGroupIcon)
+export const Wifi = h(WifiIcon)
+export const Workflow = h(ArrowPathRoundedSquareIcon)
+export const X = h(XMarkIcon)
+export const XCircle = h(XCircleIcon)
+export const Zap = h(BoltIcon)
